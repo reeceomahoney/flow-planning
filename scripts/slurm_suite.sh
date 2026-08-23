c4a66628b72a082c68d06128d610f360c1427cf6
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-08-23 17:02:02 +0100

    slurm_suite: TRAIN/EPISODES passthrough

--- scripts/slurm_suite.sh
+++ scripts/slurm_suite.sh
@@ -6,5 +6,5 @@ TASKS=${2:-"0 1 2 3"}
 LEVELS=${3:-"I II"}
 for T in $TASKS; do
   pixi run slurm run --cluster civo --name "${SUITE#safelibero_}_$T" --command \
-    "export PATH=\$HOME/.pixi/bin:\$PATH PYTHONUNBUFFERED=1; cd /users/kebl6123/flow-planning; LEVELS='$LEVELS' bash scripts/pipeline_libero.sh $SUITE $T 2>&1 | grep --line-buffered -vE 'it/s\]|Warning|WARNING|macro'"
+    "export PATH=\$HOME/.pixi/bin:\$PATH PYTHONUNBUFFERED=1; cd /users/kebl6123/flow-planning; TRAIN=${TRAIN:-1} EPISODES=${EPISODES:-10} LEVELS='$LEVELS' bash scripts/pipeline_libero.sh $SUITE $T 2>&1 | grep --line-buffered -vE 'it/s\]|Warning|WARNING|macro'"
 done
