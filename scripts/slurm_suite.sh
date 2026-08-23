943686201f9bc337f3a34b7dd35456d56b2ebd8b
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-08-23 19:50:58 +0100

    slurm_suite: run the pipeline from a job-private copy (rsync stale handles)

--- scripts/slurm_suite.sh
+++ scripts/slurm_suite.sh
@@ -6,5 +6,5 @@ TASKS=${2:-"0 1 2 3"}
 LEVELS=${3:-"I II"}
 for T in $TASKS; do
   pixi run slurm run --cluster civo --name "${SUITE#safelibero_}_$T" --command \
-    "export PATH=\$HOME/.pixi/bin:\$PATH PYTHONUNBUFFERED=1; cd /users/kebl6123/flow-planning; TRAIN=${TRAIN:-1} EPISODES=${EPISODES:-10} TAG=${TAG:-aug} LEVELS='$LEVELS' bash scripts/pipeline_libero.sh $SUITE $T 2>&1 | grep --line-buffered -vE 'it/s\]|Warning|WARNING|macro'"
+    "export PATH=\$HOME/.pixi/bin:\$PATH PYTHONUNBUFFERED=1; cd /users/kebl6123/flow-planning; cp scripts/pipeline_libero.sh /tmp/pipe_\$SLURM_JOB_ID.sh; TRAIN=${TRAIN:-1} EPISODES=${EPISODES:-10} TAG=${TAG:-aug} LEVELS='$LEVELS' bash /tmp/pipe_\$SLURM_JOB_ID.sh $SUITE $T 2>&1 | grep --line-buffered -vE 'it/s\]|Warning|WARNING|macro'"
 done
